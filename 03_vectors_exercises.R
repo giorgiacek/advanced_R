e602b706f8482c443d58d5e41a189a1d1fa9b7b2
giorgiacek <giorgivola@gmail.com>
2024-04-26 15:15:19 -0400

    some exercises added, lists and data.tables still to do

--- 03_vectors_exercises.R
+++ 03_vectors_exercises.R
@@ -6,4 +6,22 @@
 # 0. Load packages
 library(lobstr)
 
-# 1.
+# 1. Atomic
+
+# 2. Attributes
+# Question 1: What does dim() return when applied to a 1-dimensional vector?
+# When might you use NROW() or NCOL()?
+
+x <- 1:10
+nrow(x)
+ncol(x)
+
+# Treats vector as a 1-column matrix, so what will it return?
+NROW(x)
+NCOL(x)
+
+
+
+
+
+
